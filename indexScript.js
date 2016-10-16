938917054cee3c521054654ee740bfc77332de82
youhandan <handan.you.ext@nokia.com>
2016-10-16 18:28:18 +0800

    fixed bug 'after clearList can't add' and fixed some uncomfortable on view

--- indexScript.js
+++ indexScript.js
@@ -1,45 +1,104 @@
 /**
  * Created by youhandan on 2016/10/14.
  */
-$('#number').blur(function () {
-    var num=$(this).val();
-    if(num<0){
-        alert("Please input positive number!");
-        $(this).val(0);
+$(function () {
+    $('#number').blur(function () {
+        var num=$(this).val();
+        if(num<0){
+            alert("Please input positive number!");
+            $(this).val(0);
+        }
+        else{
+            $(this).val(parseInt(num));
+        }
+    });
+    $('#endTime').blur(function () {
+        if($(this).val()<$('#startTime').val()){
+            alert("Please input correct time period!");
+        }
+    });
+
+    $('#add').click(function () {
+        var date=$('#date').val();
+        var startTime=$('#startTime').val();
+        var endTime=$('#endTime').val();
+        var num=$('#number').val();
+        var listContent=$('#list').val();
+        if((startTime<endTime)&&(num!=="")){
+            if(listContent==''){
+                listContent+=date+' '+startTime+'~'+endTime+' '+num;
+                $('#list').val(listContent);
+            }
+            else{
+                listContent+='\n'+date+' '+startTime+'~'+endTime+' '+num
+                $('#list').val(listContent);
+            }
+
+        }
+        else{
+            alert('Please add correct information format!')
+        }
+
+    });
+    $('#result').click(function () {
+        var str=$('#list').val();
+        if(inputVerification(str)){
+            var resultStr=generateSummary(str);
+            $('#resultList').val(resultStr);
+        }
+        else alert('Input format is wrong!')
+
+
+    });
+    $('#clean').click(function () {
+        $('textarea').val('');
+    });
+
+    function inputVerification(str) {
+        var pattern=/^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}~\d{2}:\d{2}\s\d+$/;
+        var strArr=str.split('\n');
+        return strArr.every(function (e) {
+            if(pattern.test(e)){
+                var inputElement=e.split(' ');
+                var date=new Date(inputElement[0]);
+                var dateState=dateVerify(date);
+                var timePeriod=inputElement[1];
+                var timeState=timePeriodVerify(timePeriod);
+                var participantNum=parseInt(inputElement[2]);
+                var participantState=participantVerify(participantNum);
+                return dateState&&timeState&&participantState
+
+            }
+            else return false
+
+        })
+
     }
-    else{
-        $(this).val(parseInt(num));
+
+
+
+    function dateVerify(date) {
+        return String(date)!=='Invalid Date'
     }
-});
-$('#endTime').blur(function () {
-    if($(this).val()<$('#startTime').val()){
-        alert("Please input correct time period!");
+    function timePeriodVerify(timePeriod) {
+        var timeElement=timePeriod.split('~');
+        var timePattern=/^\d{2}:00$/;
+        return timeElement.every(function (e) {
+            if(timePattern.test(e)){
+                var startTime=parseInt(timeElement[0].match(/\d{2}/)[0]);
+                var endTime=parseInt(timeElement[1].match(/\d{2}/)[0]);
+                return endTime-startTime==2||endTime-startTime==3;
+            }
+            else return false
+        });
     }
-});
-
-$('#add').click(function () {
-    var date=$('#date').val();
-    var startTime=$('#startTime').val();
-    var endTime=$('#endTime').val();
-    var num=$('#number').val();
-    if((startTime<endTime)&&(num!=="")){
-        var str=date+' '+startTime+'~'+endTime+' '+num+'\n';
-        $('#list').append(str);
-
-    }
-    else{
-        alert('Please input correct information format!')
+    function participantVerify(participantNum){
+        var positiveInteger=/^\d+$/;
+        return positiveInteger.test(participantNum)
     }
 
-});
-$('#result').click(function () {
-    var str=$('#list').text();
-    var resultStr=generateSummary(str);
-    $('#resultList').text(resultStr);
-
-});
-$('#clean').click(function () {
-    $('textarea').val('');
-});
 
 
+
+
+});
